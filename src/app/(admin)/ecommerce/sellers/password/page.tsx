0be7acddafeac8148b9deed18e0c0dfbb85fe15d
thuybinhle <thuybinhle8@gmail.com>
2025-12-01 22:40:50 +0700

    seller change password

---
+++ src/app/(admin)/ecommerce/sellers/password/page.tsx
@@ -0,0 +1,13 @@
+import PageTitle from '@/components/PageTitle'
+import SellerChangePassword from '../components/SellerChangePassword'
+
+const SellerPasswordPage = () => {
+  return (
+    <>
+      <PageTitle title="Đổi mật khẩu" />
+      <SellerChangePassword />
+    </>
+  )
+}
+
+export default SellerPasswordPage
